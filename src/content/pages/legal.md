079f187e9519ac5601cc34daf7f053b930829d72
azahnen <zahnen@interactive-instruments.de>
2018-02-10 12:53:23 +0100

    impressum etc

--- src/content/pages/legal.md
+++ src/content/pages/legal.md
@@ -5,7 +5,48 @@ position: '20'
 sections:
   - title: Impressum
     name: legal
-    text: 
-		
+    text: >-
+        ### Angaben gemäß § 5 TMG
+
+
+        Carolin Henseler
+
+
+        AlltagsHunde
+
+
+        Huppenbergstr. 44c
+
+
+        53343 Wachtberg
+        
+
+        ### Kontakt
+
+        Telefon &#x2B;&#x34;&#x39;&#x20;&#x28;&#x30;&#x29;&#x20;&#x31;&#x37;&#x36;&#x20;&#x36;&#x31;&#x37;&#x20;&#x31;&#x31;&#x20;&#x37;&#x37;&#x32;
+
+        E-Mail &#x63;&#x61;&#x72;&#x6F;&#x40;&#x61;&#x6C;&#x6C;&#x74;&#x61;&#x67;&#x73;&#x68;&#x75;&#x6E;&#x64;&#x65;&#x2D;&#x62;&#x6F;&#x6E;&#x6E;&#x2E;&#x64;&#x65;
+
+        ### Umsatzsteuer
+
+        Alle angegebenen Preise sind Endpreise. Aufgrund des Kleinunternehmerstatus gem. § 19 UStG erheben wir keine Umsatzsteuer und weisen diese daher auch nicht aus.
+
+        ### Angaben zur Berufshaftpflichtversicherung
+
+        Name und Sitz der Gesellschaft
+
+        Haftpflichtkasse Darmstadt VVaG
+
+        Arheilger Weg 5
+
+        64380 Roßdorf
+
+        Geltungsraum der Versicherung Deutschland
+
+        ### Hinweis auf EU-Streitschlichtung
+
+        Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: [http://ec.europa.eu/consumers/odr](http://ec.europa.eu/consumers/odr)
+
+        Unsere E-Mail-Adresse finden sie oben im Impressum.
 ---
 
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -23,12 +23,24 @@ const Footer = glamorous.footer({
     display: 'flex',
     flexWrap: 'wrap', // allow us to do the line break for collapsing content
     alignItems: 'center',
-    justifyContent: 'center', //'space-between', // space out brand from logo
+    justifyContent: 'space-between', //'space-between', // space out brand from logo
     paddingTop: '5rem',
-    paddingBottom: '5rem'
+    paddingBottom: '5rem',
+    paddingLeft: '10rem',
+    paddingRight: '10rem'
 }, ({theme}) => ({
     color: theme.color.lightest,
     backgroundColor: theme.color.primary
+}));
+
+const FooterLink = glamorous(Link)({
+    textDecoration: 'none',
+    paddingLeft: '1rem',
+}, ({theme}) => ({
+    color: theme.color.lightest,
+    ':hover, &.active': {
+        color: theme.color.secondary
+    }
 }));
 
 export default ({children, data, noBanner}) => <ThemeProvider theme={ theme }>
@@ -41,7 +53,8 @@ export default ({children, data, noBanner}) => <ThemeProvider theme={ theme }>
                                                        { !noBanner && <Banner sizes={ data.file.childImageSharp.sizes } /> }
                                                        { children() }
                                                        <Footer>
-                                                           © Hundezentrum Bornheim 2017
+                                                           <span>© Hundezentrum Bornheim 2017</span>
+                                                           <span><FooterLink to="/privacy/" activeClassName="active"> Datenschutz </FooterLink><FooterLink to="/legal/" activeClassName="active"> Impressum </FooterLink></span>
                                                        </Footer>
                                                    </div>
                                                </ThemeProvider>
